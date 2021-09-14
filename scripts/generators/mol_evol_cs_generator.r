5b2623438c4aeeda5fd918d1deacba7f33fbc7c1
gwct <greggwct@gmail.com>
2021-09-14 10:14:54 -0400

    added rate page for branch leading to cam-sun

---
+++ scripts/generators/mol_evol_cs_generator.r
@@ -0,0 +1,12 @@
+############################################################
+# For rodent web, 01.20
+# This generates the file "mol_evol_cs.html"
+############################################################
+
+
+cat("Rendering mol_evol_cs.rmd/html\n")
+Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc/")
+library(rmarkdown)
+setwd("C:/bin/hamster-genome/scripts/generators")
+output_dir = "../.."
+render("../markdown/mol_evol_cs.rmd", output_dir = output_dir, params = list(output_dir = output_dir), quiet = TRUE)
